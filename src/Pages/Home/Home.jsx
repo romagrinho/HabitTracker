4397702b361eb40cfe983127cda9d473f6a6bf3b
romagrinho <romariosaguilar@gmail.com>
2024-04-02 17:49:07 -0300

    0.2

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home(){
+    return(
+        <section className="Home">
+            <div className="container-section1">
+                <header>
+                    <div>
+                        <img></img>
+                    </div>
+                        <ul>
+                            <DIV></DIV>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <ul></ul>
+                </header>
+            </div>
+            <div className="container-section2"></div>
+            <div className="container-section3"></div>
+            <div className="container-section4"></div>
+            
+        </section>
+    )
+}
